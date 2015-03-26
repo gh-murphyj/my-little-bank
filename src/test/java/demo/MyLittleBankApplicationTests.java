3e37abf2f2eeb3d9e530d4ebf95ba67abc6cb17c
John Murphy <jemnews-git@outlook.com>
2015-03-26 12:51:49 +0000

    initial

---
+++ src/test/java/demo/MyLittleBankApplicationTests.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mylittlebank.mvc.MyLittleBankApplication;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MyLittleBankApplication.class)
+@WebAppConfiguration
+public class MyLittleBankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
